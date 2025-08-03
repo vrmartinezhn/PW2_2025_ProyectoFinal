aea12b16c91fc53e5088a8069b1574855d5db3bc
EdwinGODOY <edwingodoy307@gmail.com>
2025-08-03 09:23:31 -0600

    Update Estudiante.java
    subiendo estudiante

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/Estudiante.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/Estudiante.java
@@ -1,4 +1,78 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.model;
 
-public class Estudiante {
+import java.io.Serializable;
+
+public class Estudiante implements Serializable {
+
+
+    private int id;
+    private int numero_cuenta;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String genero;
+    private String estatus;
+
+
+
+    public Estudiante() {
+        this.id = 0;
+        this.numero_cuenta = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.correo = "";
+        this.genero = "";
+        this.estatus = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero_cuenta() {
+        return numero_cuenta;
+    }
+
+    public void setNumero_cuenta(int numero_cuenta) {
+        this.numero_cuenta = numero_cuenta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {this.apellido = apellido;}
+
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {this.correo = correo;}
+
+
+
 }

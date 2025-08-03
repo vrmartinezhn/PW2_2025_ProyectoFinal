2a61d2a2555269fd34f316c2c8d4b6330dab82c3
jeisonespinal <158984918+jeisonespinal@users.noreply.github.com>
2025-08-03 01:34:50 -0600

    Update MateriasInteractor.java

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/MateriasInteractor.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/MateriasInteractor.java
@@ -1,4 +1,12 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.controller;
 
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.Materia;
+
 public interface MateriasInteractor {
+    void consultarMaterias();
+    void crearMateria(Materia materia);
+    void actualizarMateria(Materia materia);
+    void eliminarMateria(int id);
+    void eliminarMaterias(int[] ids);
 }
+

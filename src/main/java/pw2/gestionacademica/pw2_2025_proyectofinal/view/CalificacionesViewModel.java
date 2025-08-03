ada35087e20f022fc3694238386c9614a0d4c5d8
JoluiDev <124008612+JoluiDev@users.noreply.github.com>
2025-08-03 04:02:58 -0600

    Update CalificacionesViewModel.java
    Agregué calificaciones view model

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/CalificacionesViewModel.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/CalificacionesViewModel.java
@@ -1,4 +1,12 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.view;
 
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.Calificacion;
+
+import java.util.List;
+
 public interface CalificacionesViewModel {
+    void mostrarCalificacionesDataTable(List<Calificacion> calificaciones);
+    void refrescarPantalla();
+    void mostrarMensajeExito(String mensaje);
+    void mostrarMensajeError(String mensaje);
 }

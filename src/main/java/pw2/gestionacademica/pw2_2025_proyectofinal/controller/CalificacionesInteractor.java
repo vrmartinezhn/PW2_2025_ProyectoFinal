15392891bdf63c95f2c09302a2459c68f51e2c92
JoluiDev <124008612+JoluiDev@users.noreply.github.com>
2025-08-03 03:48:32 -0600

    Update CalificacionesInteractor.java
    Agregue el interactor de calificaciones

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/CalificacionesInteractor.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/CalificacionesInteractor.java
@@ -1,4 +1,11 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.controller;
 
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.Calificacion;
+
 public interface CalificacionesInteractor {
+    void consultarCalificaciones();
+    void crearCalificacion(Calificacion calificacion);
+    void actualizarCalificacion(Calificacion calificacion);
+    void eliminarCalificacion(int id);
+    void eliminarCalificaciones(int[] ids);
 }

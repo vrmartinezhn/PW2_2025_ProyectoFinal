baba091e7a018c241919e2eda22da23ab311a36a
EdwinGODOY <edwingodoy307@gmail.com>
2025-08-03 09:18:42 -0600

    Update EstudiantesInteractor.java
    actualizando estudiantesinteractor

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/EstudiantesInteractor.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/EstudiantesInteractor.java
@@ -1,4 +1,14 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.controller;
 
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.*;
 public interface EstudiantesInteractor {
+        void consultarEstudiantes();
+        void crearEstudiantes(Estudiante estudiante);
+        void actualizarEstudiantes(Estudiante estudiante);
+        void eliminarEstudiante(int id);
+        void eliminarEstudiantes(int[] ids);
+
+        // tienen que añadirlo
+        void consultarEstudiantePorId(int id);
+
 }

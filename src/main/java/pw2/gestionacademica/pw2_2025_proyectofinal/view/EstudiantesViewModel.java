3bd4cc66b330a52768ff7adbd26194247aadd5f5
EdwinGODOY <edwingodoy307@gmail.com>
2025-08-03 09:46:12 -0600

    Update EstudiantesViewModel.java
    subiendo cambios de estudiantesviewmodel

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/EstudiantesViewModel.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/EstudiantesViewModel.java
@@ -1,4 +1,11 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.view;
 
+import java.util.List;
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.*;
+
 public interface EstudiantesViewModel {
+    void mostrarEstudiantesDataTable(List<Estudiante> estudiantes);
+    void mostrarMensajeExito(String mensaje);
+    void mostrarMensajeError(String mensaje);
+    void refrescarPantalla();
 }

c0f78a52694b282c08e3096a7a460592c470a826
jeisonespinal <158984918+jeisonespinal@users.noreply.github.com>
2025-08-03 01:40:39 -0600

    Update MateriasViewModel.java

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/MateriasViewModel.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/view/MateriasViewModel.java
@@ -1,4 +1,12 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.view;
 
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.Materia;
+
+import java.util.List;
+
 public interface MateriasViewModel {
+    void mostrarMateriasDataTable(List<Materia> materias);
+    void refrescarPantalla();
+    void mostrarMensajeExito(String mensaje);
+    void mostrarMensajeError(String mensaje);
 }

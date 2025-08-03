375229d53989be58d43ff5c9372a869cabd38960
jeisonespinal <158984918+jeisonespinal@users.noreply.github.com>
2025-08-03 01:37:10 -0600

    Update MateriasResponse.java

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/MateriasResponse.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/MateriasResponse.java
@@ -1,4 +1,42 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.model;
 
+import java.util.List;
+
 public class MateriasResponse {
+    private List<Materia> items;
+    private boolean hasMore;
+    private int limit;
+    private int count;
+
+    public List<Materia> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Materia> items) {
+        this.items = items;
+    }
+
+    public boolean isHasMore() {
+        return hasMore;
+    }
+
+    public void setHasMore(boolean hasMore) {
+        this.hasMore = hasMore;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }

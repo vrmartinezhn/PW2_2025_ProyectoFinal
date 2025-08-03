32696998e3cfcb1e1bbf09427e588c9d4597a8e8
EdwinGODOY <edwingodoy307@gmail.com>
2025-08-03 09:30:39 -0600

    Update DatabaseRepository.java
    subiendo cambios de edwin

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/repository/DatabaseRepository.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/repository/DatabaseRepository.java
@@ -7,6 +7,8 @@ import pw2.gestionacademica.pw2_2025_proyectofinal.model.Profesor;
 import pw2.gestionacademica.pw2_2025_proyectofinal.model.ProfesoresResponse;
 import pw2.gestionacademica.pw2_2025_proyectofinal.model.Calificacion;
 import pw2.gestionacademica.pw2_2025_proyectofinal.model.CalificacionesResponse;
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.Estudiante;
+import pw2.gestionacademica.pw2_2025_proyectofinal.model.EstudiantesResponse;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -94,6 +96,44 @@ public interface DatabaseRepository {
     })
     @DELETE("/pls/apex/uthworkspace/appacademica/calificaciones")
     Call<ResponseBody> deleteCalificacion(@Query("id") int id);
+
+    //=====================================================================estudiante
+     @Headers({
+            "Accept: application/vnd.github.v3.full+json",
+            "User-Agent: Retrofit-Sample-App"
+    })
+    @GET("/pls/apex/uthworkspace/appacademica/estudiantes")
+    Call<EstudiantesResponse> listEstudiante();
+    @Headers({
+            "Accept: application/vnd.github.v3.full+json",
+            "User-Agent: Retrofit-Sample-App"
+    })
+    @POST("/pls/apex/uthworkspace/appacademica/estudiantes")
+    Call<ResponseBody> createEstudiante(@Body Estudiante estudiante);
+
+    @Headers({
+            "Accept: application/vnd.github.v3.full+json",
+            "User-Agent: Retrofit-Sample-App"
+    })
+    @PUT("/pls/apex/uthworkspace/appacademica/estudiantes")
+    Call<ResponseBody> updateMEstudiante(@Body Estudiante estudiante);
+
+    @Headers({
+            "Accept: application/vnd.github.v3.full+json",
+            "User-Agent: Retrofit-Sample-App"
+    })
+    @DELETE("/pls/apex/uthworkspace/appacademica/estudiantes")
+    Call<ResponseBody> deleteEstudiante(@Query("id") int id);
+
+    //para buscar estudinateporid
+    @Headers({
+            "Accept: application/vnd.github.v3.full+json",
+            "User-Agent: Retrofit-Sample-App"
+    })
+    @GET("/pls/apex/uthworkspace/appacademica/estudianteid")
+    Call<EstudiantesResponse> getEstudiantePorId(@Query("id") int id);
+
+    
     
 }
 

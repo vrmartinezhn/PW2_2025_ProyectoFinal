947d745bb2c452e351ed3391266f444ec22aa322
JoluiDev <124008612+JoluiDev@users.noreply.github.com>
2025-08-03 03:53:21 -0600

    Update CalificacionesResponse.java
    Agregué el calificacionesResponse

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/CalificacionesResponse.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/model/CalificacionesResponse.java
@@ -1,4 +1,43 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.model;
 
+import java.util.List;
+
 public class CalificacionesResponse {
+    private List<Calificacion> items;
+    private boolean hasMore;
+    private int limit;
+    private int count;
+
+    public List<Calificacion> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Calificacion> items) {
+        this.items = items;
+    }
+
+    public boolean isHasMore() {
+        return hasMore;
+    }
+
+    public void setHasMore(boolean hasMore) {
+        this.hasMore = hasMore;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
+

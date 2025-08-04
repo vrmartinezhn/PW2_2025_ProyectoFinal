c4a07b34ee91ee4da8178ec0ea0d1b9b08b60dc7
JoluiDev <124008612+JoluiDev@users.noreply.github.com>
2025-08-03 20:34:45 -0600

    Update EstudiantesInteractor.java
    Agregando/corrigiendo nuevas funciones

--- src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/EstudiantesInteractor.java
+++ src/main/java/pw2/gestionacademica/pw2_2025_proyectofinal/controller/EstudiantesInteractor.java
@@ -1,15 +1,13 @@
 package pw2.gestionacademica.pw2_2025_proyectofinal.controller;
 
 import pw2.gestionacademica.pw2_2025_proyectofinal.model.*;
+
 public interface EstudiantesInteractor {
         void consultarEstudiantes();
         void crearEstudiantes(Estudiante estudiante);
         void actualizarEstudiantes(Estudiante estudiante);
         void eliminarEstudiante(int id);
         void eliminarEstudiantes(int[] ids);
-
-        // tienen que añadirlo
         void consultarEstudiantePorId(int id);
-        void consultarEstudiantePornumero_cuenta(int numero_cuenta);
-
+        boolean consultarEstudiantePorNumero_cuenta(int numero_cuenta);
 }
